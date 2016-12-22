a8a861abd693953b77a5d4f67f2a171a64bbde5e
FirmanMFK <firman.mfk@gmail.com>
2016-12-22 20:12:21 +0700

    Adding UserTransformer

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,12 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Http\Requests;
+use App\Transformers\UserTransformer;
+use Auth;
 
 class UserController extends Controller
 {
     public function users(User $user)
     {
       $users = $user->all();
-      return response()->json($user);
+
+      return fractal()
+             ->collection($users)
+             ->transformWith(new UserTransformer)
+             ->toArray();
+
+    }
+
+    public function profile(User $user)
+    {
+      $user = $user->find(Auth::user()->id);
+        return fractal()
+            ->item($user)
+            ->transformWith(new UserTransformer)
+            ->toArray();
     }
 }
